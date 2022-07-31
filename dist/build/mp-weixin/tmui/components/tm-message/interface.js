2f3a1f035a5e18745085772a17d9e397a4e32bd3
xxv <leiliy@qq.com>
2022-07-31 21:19:05 +0800

    删除 打包文件

--- dist/build/mp-weixin/tmui/components/tm-message/interface.js
+++
@@ -1,1 +0,0 @@
-"use strict";var s=(s=>(s.load="load",s.error="error",s.info="info",s.warn="warn",s.quest="quest",s.success="success",s.disabled="disabled",s.wait="wait",s))(s||{});exports.modelType=s;
--- dist/dev/mp-weixin/theme/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const theme = {};
-exports.theme = theme;
