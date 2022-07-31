9749fbef01ce1fc2342c1eda6a667c62f71891cd
xxv <leiliy@qq.com>
2022-07-31 21:20:08 +0800

    添加 登录流程

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,10 +2,9 @@
 	<tm-app>
 		<tm-sheet>
 			<view class="flex flex-row flex-wrap">
-				<tm-button :margin="[10, 10]" size="mini" :shadow="0" label="登录"></tm-button>
-				<tm-button :margin="[10, 10]" transprent text :shadow="0" size="normal" label="签到"></tm-button>
+				<tm-button :margin="[10, 10]" :shadow="0" label="登录" @click="getUserInfo"></tm-button>
+				<tm-button :margin="[10, 10]" :shadow="0" label="签到"></tm-button>
 			</view>
-			<tm-button block label="block"></tm-button>
 		</tm-sheet>
 	</tm-app>
 </template>
@@ -18,12 +17,104 @@
 		onShow,
 		onLoad
 	} from "@dcloudio/uni-app";
-	import tmApp from "@/tmui/components/tm-app/tm-app.vue"
-	import tmSheet from "@/tmui/components/tm-sheet/tm-sheet.vue"
+	import tmApp from "../../tmui/components/tm-app/tm-app.vue"
+	import tmSheet from "../../tmui/components/tm-sheet/tm-sheet.vue"
 	import tmButton from "../../tmui/components/tm-button/tm-button.vue"
+	const name = ref('')
+	const avatar = ref('')
+	// 登录
+	function getUserInfo() {
+		wx.getUserProfile({
+			desc: '更新您的个人信息',
+			success: (res) => {
+				name.value = res.userInfo.nickName; //昵称
+				avatar.value = res.userInfo.avatarUrl; //头像
+				// 成功后进行登录,获取code
+				console.log(name, avatar);
+				wxlogin();
+			},
+			fail(err) {
+				uni.showToast({
+					icon: 'none',
+					title: '授权失败',
+					duration: 1500
+				})
+			}
+		})
+	}
 
-	const modeltype = ref('day')
-	const dateStr = ref('')
-	const showdate = ref(false)
-	const testtext = ref("哈哈")
+	function wxlogin() {
+		uni.showLoading({
+			title: '登录中...'
+		});
+		uni.login({
+			provider: 'weixin',
+			success: function(loginRes) {
+				wxloginres(loginRes.code);
+			}
+		});
+
+	}
+
+	function wxloginres(code) {
+		let that = this
+		uniCloud.callFunction({
+			name: 'user_authorize',
+			data: {
+				name: name.value,
+				avatar: avatar.value,
+				mptype: 'wx',
+				code: code,
+				userId: '62e67d840d082200016150e4',
+			},
+		}).then(res => {
+			if (res.result.success) {
+				console.log(res.result.data);
+				// that.$u.vuex('vuex_user', res.result.data)
+				// that.getUser()
+				// await that.getSign()
+				uni.showToast({
+					icon: 'none',
+					mask: true,
+					title: '登录成功',
+					duration: 1500
+				})
+			} else {
+				uni.showToast({
+					icon: 'none',
+					mask: true,
+					title: res.result.msg,
+					duration: 1500
+				})
+			}
+		})
+	}
+
+
+
+
+
+
+
+	function loginClick() {
+		signClick()
+	}
+
+	function signClick() {
+		uni.showLoading({
+			mask: true,
+			title: '签到中...'
+		})
+		uniCloud.callFunction({
+			name: 'sign_add',
+			data: {
+				"userId": ''
+			},
+		}).then(res => {
+			console.log(res);
+			uni.hideLoading()
+		})
+
+
+	}
 </script>
--- uniCloud-aliyun/cloudfunctions/user_authorize/index.js
+++ uniCloud-aliyun/cloudfunctions/user_authorize/index.js
@@ -3,101 +3,77 @@ const db = uniCloud.database();
 const dbCmd = db.command;
 const uniID = require('uni-id')
 exports.main = async (event, context) => {
-
-	let appIds
-	let appSecrets
-	if (event.mptype == 'wx') {
-		appIds = await db.collection('ls-config').where({
-			'key': 'wxId'
-		}).get()
-		appSecrets = await db.collection('ls-config').where({
-			'key': 'wxSecret'
-		}).get()
-	}
-	
-	if (appIds.data.length === 0 || appSecrets.data.length === 0) {
-		return {
-			success: false,
-			msg: '未配置登录参数'
-		}
-	}
-	const appId = appIds.data[0].val
-	const appSecret = appSecrets.data[0].val
-	console.log(appId, appSecret);
-	// 配置登录参数
-	let uniAccountIns = uniID.loginByWeixin({
-		appId: appId,
-		secret: appSecret
+	let res = await uniID.loginByWeixin({
+		code: event.code
 	})
-	
-	let res = await uniID.code2SessionWeixin(event.code);
-	return false;
 	let timeStamp = new Date().getTime()
-	let wx_user = await db.collection('wx_user').where({
+	let user = await db.collection('ls-user').where({
 		openid: res.openid
 	}).get();
-	let newdate = {};
 	// 判断用户是否存在
-	if (wx_user.data.length == 0) {
-		let newdate = {
+	if (user.data.length == 0) {
+		await db.collection('ls-user').add({
 			openid: res.openid,
 			name: event.name,
 			avatar: event.avatar,
 			integral: 0,
 			status: 1,
-			mp: event.mptype,
+			mp: res.userInfo.register_env.uni_platform,
 			time: timeStamp
-		};
-		let user_new = await db.collection('wx_user').add(newdate);
+		});
 		// 判断是否是被邀请用户
 		if (event.userId) {
-			let integrals = await db.collection('wx_config').where({
+			//获取邀请积分
+			let integrals = await db.collection('ls-config').where({
 				'key': 'inviteIntegral'
 			}).get()
+
 			if (integrals.data.length > 0) {
 				const integral = parseInt(integrals.data[0].val)
-
-				var userList = await db.collection('wx_user').doc(event.userId).get()
+				var userList = await db.collection('ls-user').doc(event.userId).get()
+				//判断 邀请用户 是否存在
 				if (userList.data.length > 0) {
-					// 给用户增加积分
-					var userResult = await db.collection('wx_user').doc(event.userId).update({
+					// 给用户增加积分 inc自增积分
+					db.collection('ls-user').doc(event.userId).update({
 						'integral': dbCmd.inc(integral)
+					}).then(res => {
+						if (res.updated === 1) {
+							// 添加积分记录
+							let xxx = db.collection('ls-record').add({
+								'mode': 1,
+								'name': '邀请 ' + event.name + ' 奖励积分',
+								'integral': integral,
+								'user_id': event.userId,
+								'time': timeStamp
+							})
+						}
 					})
-					if (userResult.updated === 1) {
-						// 添加积分记录
-						var signResult = await db.collection('wx_record').add({
-							'mode': 1,
-							'name': '邀请 ' + event.name + ' 奖励积分',
-							'integral': integral,
-							'user_id': event.userId,
-							'time': timeStamp
-						})
-					}
 				}
 			}
 		}
 	} else {
-		let user_update = await db.collection('wx_user').where({
+		await db.collection('ls-user').where({
 			openid: res.openid
 		}).update({
 			'name': event.name,
 			'avatar': event.avatar
 		})
 	}
-
 	// 查询最新的数据
-	let result = await db.collection('wx_user').where({
+	db.collection('ls-user').where({
 		openid: res.openid
-	}).get();
-	if (result.data.length > 0) {
-		return {
-			success: true,
-			data: result.data[0]
+	}).get().then(res => {
+		if (res.data.length > 0) {
+			return {
+				success: true,
+				data: res.data[0]
+			}
+		} else {
+			return {
+				success: false,
+				msg: '用户不存在'
+			}
 		}
-	} else {
-		return {
-			success: false,
-			msg: '用户不存在'
-		}
-	}
+	})
+
 };
