3c9ec030bbc940bb87fd66087cc01a4c70890c28
LeeZi <35288214+xianyu21@users.noreply.github.com>
2022-08-03 15:21:52 +0800

    更新 tmui

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script  lang="ts">
-	
+
 </script>
 <style>
+	
 	/* #ifdef APP-NVUE */
 	@import './tmui/scss/nvue.css';
 	/* #endif */
 	/* #ifndef APP-NVUE */
 	@import './tmui/scss/noNvue.css';
 	/* #endif */
+
+
 	/* #ifdef H5 */
 	body::-webkit-scrollbar,
 	div::-webkit-scrollbar,
@@ -18,6 +21,10 @@
 	body.pages-index-index uni-page-body,
 	body {
 		padding-bottom: 0 !important;
+		
+	}
+	text{
+		font-family:"sans-serif";
 	}
 	/* #endif */
 </style>
